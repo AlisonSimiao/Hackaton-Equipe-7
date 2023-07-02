09d1a16f208f0e8066d15096cef1a574f023240f
Alison Simião <alison.s@multiplier.com.br>
2023-07-02 15:48:10 -0300

    feat: add response de update

--- src/response/index.response.js
+++ src/response/index.response.js
@@ -15,11 +15,17 @@ function FindOne(data, res) {
     return res.status(200).json(data)
 }
 
+function Update(data, res) {
+    return res.status(200).json(data)
+}
+
 function Delete(res) {
     return res.status(204)
 }
 
 function ErrorResponse(error, res) { 
+    console.error(error)
+    
     error.statusCode 
     ? res.status(error.statusCode).json({message: error.message}) 
     : res.status(500).json({message: 'Erro interno'})
@@ -31,5 +37,6 @@ module.exports = {
     Create,
     Delete,
     FindOne,
+    Update,
     ErrorResponse,
 }
