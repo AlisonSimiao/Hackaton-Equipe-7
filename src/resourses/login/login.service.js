b74a6d34d6fe07f551942430d94487a4f50b32f0
Alison Simião <alison.s@multiplier.com.br>
2023-07-02 10:30:28 -0300

    feat(login): add retono

--- src/resourses/login/login.service.js
+++ src/resourses/login/login.service.js
@@ -2,20 +2,28 @@ const crypto = require('crypto');
 import * as res from 'express/lib/response';
 
 class LoginService{
-    create(body){
+    async create(body){
         const {email, password} = body;
         const user = req.usuario
 
-        if(email !== user.email){
+        if(email !== user.email)
             return res.status(401).json({message: "Senha Ou email incorretos"})
-        }
-
+        
         const validatePass = verificarSenha(password, user.password)
 
-        if(!validatePass){
+        if(!validatePass)
             return res.status(401).json({ message: "Senha Ou email incorretos"})
+        
+
+        const token = await jwt.sign({ id: user.id }, process.env.SECRET)
+
+        return {
+            token,
+            user 
         }
+
     }
+    
 
     verificarSenha(senha, senhaEncriptada, salt) {
         const hash = crypto.pbkdf2Sync(senha, salt, 10000, 64, 'sha512').toString('hex');
