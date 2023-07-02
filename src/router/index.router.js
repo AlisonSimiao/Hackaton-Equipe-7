14ad53d42a4fca213f057ff19068b7bab7bf3f0a
Alison Simião <alison.s@multiplier.com.br>
2023-07-02 10:30:39 -0300

    add gender

--- src/router/index.router.js
+++ src/router/index.router.js
@@ -3,6 +3,6 @@ const { Router } = require("express");
 const router = Router()
 
 router.use('/login', require('./login.router'))
-router.use('/signup', require('./signup'))
-
+//router.use('/signup', require('./signup.router.js'))
+router.use('/gender', require('./gender.router'))
 module.exports = router;
